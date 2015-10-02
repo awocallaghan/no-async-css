543cd0e495d782c7a2c4948ff0555ac7ba75dbc7
Alex O'Callaghan <aocallaghan@outlook.com>
2015-10-02 16:57:16 +0100

    Added crime map, JavaScript banner and more colour

--- _posts/2015-09-04-hello-world.md
+++ _posts/2015-09-04-hello-world.md
@@ -1,12 +1,8 @@
 ---
 layout: post
 title: Hello world!
+pubDate: "4 Sep 2015"
 keywords: hello world, example blog post
 description: Hello world! This is an example blog post first created to test Jekyll
 ---
-{{ page.title }}
-=================
-
-4 Sep 2015 - Bournemouth
-
 Hello world! This the first blog post on my personal website powered by Jekyll and GitHub pages!
---
+++ _site/js/banner.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+  var BOX_WIDTH = 50;
+  var width;
+
+  window.addEventListener('resize', updateWidth); // add event to change number of boxes when window resized
+
+  updateWidth(); // manually update width when page is ready
+
+  // Update the current width then update boxes shown
+  function updateWidth() {
+    width = $(".banner").width();
+    console.log("Width = " + width);
+    removeBoxes();
+    addBoxes();
+  }
+
+  // Remove boxes by emptying the .banner
+  function removeBoxes() {
+    $(".banner").html(" ");
+  }
+
+  // Add .banner-box(es) to the .banner using width to calculate number of boxes to add
+  function addBoxes() {
+    var boxes = Math.floor(width / BOX_WIDTH); // calculate number of boxes to add to banner
+    var remainder = Math.floor(width % BOX_WIDTH); // remainder is size of additional first box
+    if (remainder != 0) {
+      $(".banner").append("<div class=\"banner-box\" style=\"width: " + remainder + "px;\"></div>");
+    }
+    for (var i = 0; i < boxes; i++) {
+      var duration = Math.round(Math.random() * 10) + 5; // get random number between 5 and 15 for length of animation
+      $(".banner").append("<div class=\"banner-box\" style=\"animation-duration: " + duration + "s;\"></div>");
+    }
+  }
+
+});
