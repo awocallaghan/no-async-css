646f1a736eb0164677e7ea67f6ab298c1062127c
Alex O'Callaghan <aocallaghan@outlook.com>
2017-07-12 16:29:51 +0100

    Moved tracker token to data object

--- js/video-analytics/init-tracker.js
+++ js/video-analytics/init-tracker.js
@@ -6,9 +6,9 @@
 
 var tracker = new window.tracker({
   debug: true,
-  token: "11c9af04-bc31-4390-a4af-c9f258cc1d3b",
   events: ["loadedmetadata", "play", "pause", "seeking", "seeked", "volumechange", "timeupdate"],
   data: {
+    token: "11c9af04-bc31-4390-a4af-c9f258cc1d3b",
     url: "//video-analytics.herokuapp.com/log",
   },
 });
