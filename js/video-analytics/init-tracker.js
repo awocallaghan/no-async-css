be8bf5b7db38e4c661fb32dd93c359ef9a377749
Alex O'Callaghan <aocallaghan@outlook.com>
2017-07-12 13:59:43 +0100

    Video analytics prep

---
+++ js/video-analytics/init-tracker.js
@@ -0,0 +1,15 @@
+'use strict';
+
+/**
+ * Initialise the video tracker
+ */
+
+var tracker = new window.tracker({
+  token: "11c9af04-bc31-4390-a4af-c9f258cc1d3b",
+  events: ["loadedmetadata", "play", "pause", "seeking", "seeked", "volumechange", "timeupdate"],
+  data: {
+    url: "//video-analytics.herokuapp.com/log",
+  },
+});
+tracker.init();
+window.tracker = tracker;
