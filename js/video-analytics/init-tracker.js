08cf285c8d1ee55e1b81573002671cec3d239986
Alex O'Callaghan <aocallaghan@outlook.com>
2017-07-12 14:04:04 +0100

    Init tracker changes

--- js/video-analytics/init-tracker.js
+++ js/video-analytics/init-tracker.js
@@ -5,6 +5,7 @@
  */
 
 var tracker = new window.tracker({
+  debug: true,
   token: "11c9af04-bc31-4390-a4af-c9f258cc1d3b",
   events: ["loadedmetadata", "play", "pause", "seeking", "seeked", "volumechange", "timeupdate"],
   data: {
